3028a7b21afb7a27fc9da2afae5f2021c02a256d
qihong1 <qihong1@lenovo.com>
2019-11-27 10:00:33 +0800

    fdfdf

--- pages/test1.js
+++ pages/test1.js
@@ -288,7 +288,7 @@ class Test1 extends Component {
     return (
       <div>
         <Head title="Home" />
-        <input type="file" accept="image/*" capture="camera" multiple />
+        <input type="file" accept="image/*" capture="user" />
         <input type="file" accept="video/*" capture="camcorder" />
         <input type="file" accept="audio/*" capture="microphone" />
         {/* <Nav /> */}
